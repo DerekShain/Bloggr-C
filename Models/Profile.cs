d3e2735130d0410fcb18c2363bbe3884e9dcec44
Derek Shain <derek0anthony@gmail.com>
2021-10-26 11:31:28 -0600

    req

--- Models/Profile.cs
+++ Models/Profile.cs
@@ -1,8 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Bloggr.Models
 {
   public class Profile
   {
     public string Id { get; set; }
+    [Required]
     public string Name { get; set; }
     public string Picture { get; set; }
   }
